5632280321acf1a4971c8e2ccd986b78dfba6577
Ad3 <adrien.raulot@epitech.eu>
2014-10-16 19:10:02 +0200

    Add installation script

---
+++ install.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+printf "Warning : it will replace your actual vim configuration !\n\nProceed ?(y/n) "
+read r
+if [[ "$r" == "y" ]]; then
+    printf "Copying vim configuration...done.\n\n"
+    cp -r .vim* ~/
+    printf "****** Headers (.c) ******\n\nEnter your login : "
+    read login
+    printf "\nEnter your name : "
+    read name
+    printf "\nEnter your email : "
+    read email
+    printf "\n\nIf you want to use .c headers, add these lines in your .bashrc:"
+    printf "\nexport VIM_NICK=\"$login\"\nexport VIM_NAME=\"$name\"\nexport VIM_EMAIL=\"$email\"\n\nDone.\n"
+else
+    exit 1
+fi
+exit 0
