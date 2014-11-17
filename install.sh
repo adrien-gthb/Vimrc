b7320e5c4c196b557599702be3f71b979ecea6c6
Ad3 <adrien.raulot@epitech.eu>
2014-11-17 14:46:29 +0100

    Improve install script

--- install.sh
+++ install.sh
@@ -1,10 +1,15 @@
 #! /bin/bash
-printf "Warning : it will replace your actual vim configuration !\n\nProceed ?(y/n) "
+printf "Warning : it will replace your actual vim configuration !\n\nProceed ? (y/n) "
 read r
 if [[ "$r" == "y" ]]; then
-    printf "Copying vim configuration...done.\n\n"
-    cp -r .vim* ~/
-    printf "****** Headers (.c) ******\n\nEnter your login : "
+    printf "Copying vim configuration...\n\n"
+    if cp -r .vim* ~/; then
+        printf "done.\n\n"
+    else
+        printf "/!\ Error /!\ \n\n"
+        exit 2
+    fi
+    printf "****** Headers (.c & Makefile) ******\n\nEnter your login : "
     read login
     printf "\nEnter your name : "
     read name
